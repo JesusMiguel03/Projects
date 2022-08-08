1e3ac84ed2e91f0e2598445632e7584bd58150d5
Jesus <jmdefreitas10@gmail.com>
2022-08-08 13:28:37 -0400

    mobile fixed, number range added

--- assets/components/paint/index.js
+++ assets/components/paint/index.js
@@ -1,15 +1,27 @@
 const canvas = document.getElementById("canvas")
 const dif = canvas.getBoundingClientRect()
 const ctx = canvas.getContext("2d")
+const clearBtn = document.getElementById("clear")
+const number = document.getElementById("lineNumber") 
 
-let painting, color, linewidth, difX, difY, ejeX, ejeY;
+let painting, color, linewidth, difX, difY, ejeX, ejeY, heightRatio = 1.5;
 
+// Clear screen
+clearBtn.addEventListener("click", ()=> {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+// Responsive canvas
+canvas.height = canvas.width * heightRatio
+
+// Using mouse
 canvas.addEventListener("mousedown", e=> {
     difX = e.clientX - dif.left
     difY = e.clientY - dif.top
     painting = true
     color = document.getElementById("color").value
     linewidth = document.getElementById("lw").value
+    number.value = linewidth
     ctx.beginPath()
 })
 
@@ -34,34 +46,26 @@ const dibujar = (x1, y1, x2, y2) => {
     ctx.stroke()
 }
 
-
-// Mobile
+// Using mobile
 canvas.addEventListener("touchstart", function(e) {
-    console.log("touchstart")
     ejeX = e.touches[0].clientX - dif.left
     ejeY = e.touches[0].clientY - dif.top
-    console.log(dif.left, dif.top)
-    console.log(e.touches[0].clientX, e.touches[0].clientY)
-    console.log(ejeX, ejeY)
     painting = true
     color = document.getElementById("color").value
     linewidth = document.getElementById("lw").value
+    number.value = linewidth
     ctx.beginPath()
 })
 
 canvas.addEventListener("touchmove", function(e) {
     if (painting) {
         dibujartouch(ejeX, ejeY, e.changedTouches[0].clientX  - dif.left, e.changedTouches[0].clientY - dif.top)
-        ejeX = e.changedTouches[0].clientX  - dif.left
-        ejeY = e.changedTouches[0].clientY  - dif.top
+        ejeX = e.changedTouches[0].clientX - dif.left
+        ejeY = e.changedTouches[0].clientY - dif.top
     }
 })
 
 canvas.addEventListener("touchend", function(e) {
-    let deltaX
-    let deltaY
-    deltaX = e.changedTouches[0].clientX - ejeX
-    deltaY = e.changedTouches[0].clientY - ejeY
     ctx.closePath()
     painting = false
 })
